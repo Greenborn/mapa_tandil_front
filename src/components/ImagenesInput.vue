8ebd7e002d3906c9f66ecb8caa8713f3d4117073
Luciano <lucho.2012.tandil@gmail.com>
2024-10-12 22:43:22 -0300

    Componente de seleccion de archivos permite quitar imagenes; se obtiene base64 de los archivos

--- src/components/ImagenesInput.vue
+++ src/components/ImagenesInput.vue
@@ -1,15 +1,19 @@
 <template>
     <div class="row p-2">
         <div class="col">
-            <input type="file" id="myfile" name="myfile" 
-                accept=".png, .jpg, .jpeg .webp" multiple="multiple"
+            <input type="file" id="myfile" name="myfile" accept=".png, .jpg, .jpeg .webp" multiple="multiple"
                 @change="archivo_seleccionado">
         </div>
     </div>
 
     <div class="row">
-        <div class="col-12 p-1" v-for="(file, index) in archivos" :key="index">
-            <img class="w-100" :src="file.src" />
+        <div class="col-12" v-for="(file, index) in archivos" :key="index">
+            <div class="row">
+                <div class="col p-1">
+                    <img class="w-100" :src="file.src" />
+                    <button type="button" class="btn-rel btn btn-danger" @click="archivos.splice(index, 1)">Quitar</button>
+                </div>
+            </div>
         </div>
     </div>
 
@@ -20,14 +24,28 @@ import { ref } from 'vue';
 
 const archivos = ref([])
 
-function archivo_seleccionado( evnt ){
+function archivo_seleccionado(evnt) {
     archivos.value = []
     let files = evnt.target.files
 
-    for (let i=0; i<files.length; i++){
-        archivos.value.push({
-            src: URL.createObjectURL(files[i]),
-        })
+    for (let i = 0; i < files.length; i++) {
+        let reader = new FileReader()
+
+        reader.readAsDataURL(files[i])
+        reader.onload = async () => {
+            archivos.value.push({
+                src: URL.createObjectURL(files[i]),
+                base64: String(reader.result)
+            })
+        }
     }
 }
 </script>
+
+<style scoped>
+.btn-rel{
+    position: relative;
+    left: 1rem;
+    bottom: 3rem;
+}
+</style>
