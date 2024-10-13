0399eb1b4a3efa43230e7363d269fc3bf9d5332d
Luciano <lucho.2012.tandil@gmail.com>
2024-10-13 03:05:08 -0300

    Se genera un marcador por cada reclamo del listado

--- src/components/MapaGral.vue
+++ src/components/MapaGral.vue
@@ -1,8 +1,8 @@
 <template>
-    <ToastsCtrl ref="toasts_ref" @btn_click="toast_btn_click"/>
+    <ToastsCtrl ref="toasts_ref" @btn_click="toast_btn_click" />
     <div class="cont-modal">
-        <ol-map :loadTilesWhileAnimating="true" :loadTilesWhileInteracting="true" 
-            style="height: calc(100vh - 3.5rem)" ref="map_ref">
+        <ol-map :loadTilesWhileAnimating="true" :loadTilesWhileInteracting="true" style="height: calc(100vh - 3.5rem)"
+            ref="map_ref">
             <ol-view ref="view" :center="center" :rotation="rotation" :zoom="zoom" :projection="projection" />
 
             <ol-tile-layer><ol-source-osm /></ol-tile-layer>
@@ -11,8 +11,15 @@
                 <ol-source-vector ref="vectorsource" :projection="projection">
                     <ol-feature v-for="marcador in marcadores" :key="marcador">
                         <ol-geom-point :coordinates="marcador?.coordinate"></ol-geom-point>
-                        <ol-style>
+                        <!--<ol-style>
                             <ol-style-icon :src="marcador?.icon" :scale="marcador?.size"></ol-style-icon>
+                        </ol-style>-->
+                        <ol-style>
+                            <ol-style-stroke color="red" :width="2"></ol-style-stroke>
+                            <ol-style-fill color="rgba(255,255,255,0.1)"></ol-style-fill>
+                            <ol-style-circle :radius="7">
+                                <ol-style-fill color="red"></ol-style-fill>
+                            </ol-style-circle>
                         </ol-style>
                     </ol-feature>
 
@@ -48,7 +55,7 @@ import ToastsCtrl from './ToastsCtrl.vue';
 import { get_reclamos, new_reclamo } from '@/api/reclamos'
 
 defineExpose({ update_context })
-const emit  = defineEmits(['navigate'])
+const emit = defineEmits(['navigate'])
 
 const context = ref({
     "ultimo_enlace": null,
@@ -73,9 +80,9 @@ const ultimo_punto = ref(null);
 
 const marcadores = ref([])
 
-function new_reclamo_p1(){
+function new_reclamo_p1() {
     if (ultimo_punto.value !== null)
-        vectorsource.value.source.removeFeature( ultimo_punto.value )
+        vectorsource.value.source.removeFeature(ultimo_punto.value)
 
     toasts_ref.value.present({
         title: 'Nuevo Reclamo', title_small: 'Paso 1',
@@ -86,7 +93,7 @@ function new_reclamo_p1(){
     drawEnable.value = true
 }
 
-function new_reclamo_p2(){
+function new_reclamo_p2() {
     drawEnable.value = false
     context.value.proceso_paso_actual = 2
     toasts_ref.value.present({
@@ -97,7 +104,7 @@ function new_reclamo_p2(){
 }
 
 async function update_context(contexto) {
-    context.value = {...context.value, ...contexto}
+    context.value = { ...context.value, ...contexto }
     const ultimo_enlace = context.value?.ultimo_enlace
     context.value.proceso_actual = ultimo_enlace?.id
     //console.log(ultimo_enlace?.id)
@@ -105,14 +112,15 @@ async function update_context(contexto) {
         new_reclamo_p1()
     } else {
         if (ultimo_punto.value !== null)
-            vectorsource.value.source.removeFeature( ultimo_punto.value )
+            vectorsource.value.source.removeFeature(ultimo_punto.value)
         toasts_ref.value.dissmiss()
-    }  
-    
-    if (context.value?.action === "NEW_RECLAMO" && context.value?.form_reclamo_data){
+    }
+
+    if (context.value?.action === "NEW_RECLAMO" && context.value?.form_reclamo_data) {
+        context.value.form_reclamo_data['posicion'] = ultima_posicion.value
         let res_ = await new_reclamo(context.value?.form_reclamo_data)
-        if (res_.stat){
-            console.log(res_)
+        if (res_.stat) {
+            await update_reclamos()
             alert("Reclamo subido correctamente.")
         } else {
             alert(res_.error)
@@ -120,34 +128,43 @@ async function update_context(contexto) {
     }
 }
 
-function toast_btn_click(evnt){
-    if (context.value.proceso_actual === "NEW_RECLAMO"){
+function toast_btn_click(evnt) {
+    if (context.value.proceso_actual === "NEW_RECLAMO") {
         if (evnt)
             new_reclamo_p3()
-        else 
+        else
             new_reclamo_p1()
     }
 }
 
-function new_reclamo_p3(){
+function new_reclamo_p3() {
     toasts_ref.value.dissmiss()
     context.value.proceso_paso_actual = 3
-    emit('navigate', { id:'RECLAMO_PASO_3' })
+    emit('navigate', { id: 'RECLAMO_PASO_3' })
 }
 
 function drawend(event) {
     ultimo_punto.value = event.feature
     ultima_posicion.value = event.feature.values_.geometry.flatCoordinates
-    
-    if (context.value.proceso_actual === "NEW_RECLAMO"){
+
+    if (context.value.proceso_actual === "NEW_RECLAMO") {
         new_reclamo_p2()
     }
 }
 
-onMounted(async () => {
+async function update_reclamos(){
+    marcadores.value = []
     let res = await get_reclamos()
-    if (res.stat){
-        console.log(res)
+    if (res.stat) {
+        for (let i=0; i < res.data.length; i++) {
+            let reclamo = res.data[i]
+            //console.log(reclamo)
+            marcadores.value.push({...reclamo, coordinate: JSON.parse(reclamo.posicion) })
+        }
     }
+}
+
+onMounted(async () => {
+    await update_reclamos()
 })
 </script>
