f981510fe958396b908358b30cc8de29ff93b19d
Luciano <lucho.2012.tandil@gmail.com>
2024-10-14 19:15:07 -0300

    Menu greneral resalta items activos

--- src/components/MenuGral.vue
+++ src/components/MenuGral.vue
@@ -3,8 +3,8 @@
         <div class="row justify-content-center">
             <div class="col-auto">
                 <div class="btn-group btn-group-sm" role="group">
-                    <button type="button" v-for="enlace in ENLACES" :key="enlace.id" 
-                        @click="emit('btn_click', enlace)" :class="enlace.class">{{ enlace.label }}</button>
+                    <button type="button" v-for="(enlace, i) in ENLACES" :key="enlace.id" 
+                        @click="click_evnt(enlace, i)" :class="css_class(enlace, i)">{{ enlace.label }}</button>
                 </div>
             </div>
         </div>
@@ -12,12 +12,25 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
+
 const ENLACES = [
     { label: 'Mapa', icon: '', class: 'btn btn-outline-primary', id: 'MAP' },
     { label: '¿Cómo se usa?', icon: '', class: 'btn btn-outline-primary', id: 'HELP' },
     { label: '+ Reclamo', icon: '', class: 'btn btn-outline-success', id: 'NEW_RECLAMO' },
     //{ label: '+ Evento', icon: '', class: 'btn btn-outline-success', id: 'NEW_EVENTO' },
 ]
+
+const selected = ref(0)
+
+function css_class(enlace, i){
+    return enlace.class + (i == selected.value ? ' active' : '')
+}
+
+function click_evnt(enlace, i){
+    selected.value = i
+    return emit('btn_click', enlace)
+}
 
 const emit  = defineEmits(['btn_click'])
 </script>
